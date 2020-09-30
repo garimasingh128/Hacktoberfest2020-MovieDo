f2bd1d3486741536577decb6a9bc80758611ed7c
Rajat Jain <rjrox.21@gmail.com>
2020-09-30 11:50:57 +0000

    added a move

---
+++ build/precache-manifest.8a132d6ed293128942e8bae0139bb5b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f88eb9ec8d0a58819bab9d52cbee387",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19bdfc3457dd234d8cf6",
+    "url": "/static/css/2.c1a89a62.chunk.css"
+  },
+  {
+    "revision": "8b4f9568272c2646f5d4",
+    "url": "/static/css/main.0ca9f002.chunk.css"
+  },
+  {
+    "revision": "19bdfc3457dd234d8cf6",
+    "url": "/static/js/2.0178ae2d.chunk.js"
+  },
+  {
+    "revision": "8b4f9568272c2646f5d4",
+    "url": "/static/js/main.cb34b805.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
